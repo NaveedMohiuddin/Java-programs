eec86df36460a04beb9c6dcb5ef67efdf7bf34c8
NaveedMohiuddin <naveed.mohiuddin.1234@gmail.com>
2024-07-10 13:01:59 -0500

    10

---
+++ recursion5.java
@@ -0,0 +1,23 @@
+// program to print the all substrings of a string
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String curr="";
+        int i=0;
+        fun(s,curr,i);
+	   
+	}
+	
+	static void fun(String s,String curr, int i){
+	    if(s.length()==i){
+	        System.out.print(curr);
+	        return;
+	    }
+	    fun(s,curr,i+1);
+	    fun(s,curr+s.charAt(i),i+1);
+	}
+		
+}
